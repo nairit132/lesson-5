4257b1bde66ac7c0080a3a89a402821c446b60d5
nairit132 <nairithmajmundar@gmail.com>
2025-02-23 17:45:33 +0000

    Add files via upload

---
+++ activity two.py
@@ -0,0 +1,6 @@
+money = int(input("What was the money you started with?"))
+PorL = int(input("that was the money that you ended up with?"))
+if (PorL < money):
+    print("You have earned a loss")
+else:
+    print("you have earned money")
